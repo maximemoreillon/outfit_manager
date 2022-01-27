8bc62d08f2cc3b8b2e1d25cb91fd2e44450c39e9
Maxime Moreillon <m.moreillon@gmail.com>
2022-01-27 14:14:56 +0000

    more properties

--- models/garment.js
+++ models/garment.js
@@ -3,9 +3,11 @@ const { Schema, model } = require('mongoose')
 
  const garmentSchema = new Schema({
    user_id: String,
-   label: String,
    image: String,
+   label: String,
    color: String,
+   brand: String,
+   description: String,
  })
 
  const Garment = model('Garment', garmentSchema)
