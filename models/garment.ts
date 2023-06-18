99bcca6ff5c2d59d03334dad9b1da7ddb7734790
Maxime Moreillon <m.moreillon@gmail.com>
2023-06-18 10:15:23 +0900

    garment type property

--- models/garment.ts
+++ models/garment.ts
@@ -1,17 +1,15 @@
-import { Schema, model } from 'mongoose'
+import { Schema, model } from "mongoose"
 
+const schema = new Schema({
+  user_id: String,
+  image: String,
+  color: String,
+  brand: String,
+  description: String,
+  comment: String,
+  label: String, // What is this for?
+  tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+  type: String, // Temporary test
+})
 
- const schema = new Schema({
-   user_id: String,
-   image: String,
-   color: String,
-   brand: String,
-   description: String,
-   comment: String,
-   label: String, // What is this for?
-   tags: [{ type: Schema.Types.ObjectId, ref: 'Tag' }]
- })
-
- export default model('Garment', schema)
-
-
+export default model("Garment", schema)
