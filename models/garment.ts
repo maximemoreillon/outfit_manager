920243650e141da432104d7775017a6d8f77261c
Maxime Moreillon <m.moreillon@gmail.com>
2024-03-31 08:20:17 +0900

    minor

--- models/garment.ts
+++ models/garment.ts
@@ -4,17 +4,20 @@ const schema = new Schema({
   user_id: String,
   label: String, // A.k.a "name" or "short description"
   description: String,
-  image: String,
+
+  image: String, // Would be nice to have multiple images
   color: String,
   brand: String,
   comment: String,
-  tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
-  type: String,
+  type: String, // Would need to support multiple types, or categories
   size: String,
   quantity: { type: Number, default: 1 },
   registration_date: { type: Date, default: Date.now() },
   rating: Number,
-  condition: { type: Number, default: 100 },
+  condition: { type: Number, default: 100 }, // Percentage
+
+  // Currently unused
+  tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
 })
 
 export default model("Garment", schema)
