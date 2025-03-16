868a03c1cf0a436b518a3199f0cf3208fd886b85
Maxime Moreillon <m.moreillon@gmail.com>
2025-03-16 17:11:50 +0900

    auth

--- app/garments/page.tsx
+++ app/garments/page.tsx
@@ -1,19 +1,25 @@
 import GarmentPreviewCard from "@/components/garments/previewCard";
 import ServerPagination from "@/components/serverPagination";
 import { Button, buttonVariants } from "@/components/ui/button";
+import { auth } from "@/auth";
 import { readGarments } from "@/lib/garments";
 import Link from "next/link";
+
 export default async function Garments({
   searchParams,
 }: {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
+  const session = await auth();
+  if (!session?.user) return null;
+
   const {
     items: garments,
     total,
     offset,
     limit,
   } = await readGarments(await searchParams);
+
   return (
     <div>
       <h2 className="text-2xl my-4">Garments</h2>
---
+++ app/login/page.tsx
@@ -0,0 +1,14 @@
+import { signIn } from "@/auth";
+
+export default function Login() {
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signIn("auth0");
+      }}
+    >
+      <button type="submit">Signin with Auth0</button>
+    </form>
+  );
+}
