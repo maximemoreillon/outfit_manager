da2dead8359399dd91dfd134a6adf64f6fea42af
Maxime Moreillon <m.moreillon@gmail.com>
2025-04-17 11:05:04 +0900

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 import Link from "next/link";
+import { Shirt, PersonStanding } from "lucide-react";
 export default async function Garments() {
+  const className =
+    "flex flex-col justify-center items-center flex-grow-1 border gap-2 p-4";
   return (
-    <div>
-      <ul>
-        <li>
-          <Link href="/garments">Garments</Link>
-        </li>
-        <li>
-          <Link href="/outfits">Outfits</Link>
-        </li>
-      </ul>
+    <div className="flex gap-4">
+      <Link href="/garments" className={className}>
+        <Shirt size={50} />
+        <span>Garments</span>
+      </Link>
+
+      <Link href="/outfits" className={className}>
+        <PersonStanding size={50} />
+        <span>Outfits</span>
+      </Link>
     </div>
   );
 }
