90571cda5fd4352f94b2a5991a0e009506dc01ef
Maxime Moreillon <m.moreillon@gmail.com>
2025-04-18 10:53:27 +0900

    dynamic previewcard wrapper

--- components/garments/previewCard.tsx
+++ components/garments/previewCard.tsx
@@ -10,6 +10,7 @@ import {
 import { Button, buttonVariants } from "../ui/button";
 import Link from "next/link";
 import { garmentsTable } from "@/db/schema";
+import React from "react";
 
 type Props = {
   garment: typeof garmentsTable.$inferSelect;
@@ -17,56 +18,62 @@ type Props = {
   onRemove?: (garment: typeof garmentsTable.$inferSelect) => void;
 };
 
+// href={`/garments/${props.garment.id}`}
+
 export default function GarmentPreviewCard(props: Props) {
+  const isLink = !props.onRemove && !props.onSelect;
+  const Wrapper = isLink ? Link : React.Fragment;
   return (
-    // <Link href={`/garments/${props.garment.id}`}>
-    <Card>
-      <CardHeader>
-        <CardTitle>{props.garment.name}</CardTitle>
-        <CardDescription>{props.garment.description}</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <img
-          className="size-32"
-          src={
-            props.garment.image
-              ? `/api/images/${props.garment.image}`
-              : "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1200px-No_image_available.svg.png"
-          }
-          alt=""
-        />
-      </CardContent>
+    <Wrapper href={`/garments/${props.garment.id}`}>
+      <Card>
+        <CardHeader>
+          <CardTitle>{props.garment.name}</CardTitle>
+          <CardDescription>{props.garment.description}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <img
+            className="size-32"
+            src={
+              props.garment.image
+                ? `/api/images/${props.garment.image}`
+                : "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1200px-No_image_available.svg.png"
+            }
+            alt=""
+          />
+        </CardContent>
 
-      <CardFooter className="flex gap-2">
-        {props.onSelect && (
-          <Button
-            onClick={() => {
-              if (props.onSelect) props.onSelect(props.garment);
-            }}
-          >
-            Select
-          </Button>
-        )}
+        <CardFooter className="flex gap-2">
+          {(props.onSelect || props.onRemove) && (
+            <Link
+              href={`/garments/${props.garment.id}`}
+              className={buttonVariants({})}
+            >
+              See
+            </Link>
+          )}
 
-        <Link
-          href={`/garments/${props.garment.id}`}
-          className={buttonVariants({})}
-        >
-          See
-        </Link>
+          {props.onSelect && (
+            <Button
+              onClick={() => {
+                if (props.onSelect) props.onSelect(props.garment);
+              }}
+            >
+              Select
+            </Button>
+          )}
 
-        {props.onRemove && (
-          <Button
-            variant="destructive"
-            onClick={() => {
-              if (props.onRemove) props.onRemove(props.garment);
-            }}
-          >
-            Remove
-          </Button>
-        )}
-      </CardFooter>
-    </Card>
-    // </Link>
+          {props.onRemove && (
+            <Button
+              variant="destructive"
+              onClick={() => {
+                if (props.onRemove) props.onRemove(props.garment);
+              }}
+            >
+              Remove
+            </Button>
+          )}
+        </CardFooter>
+      </Card>
+    </Wrapper>
   );
 }
