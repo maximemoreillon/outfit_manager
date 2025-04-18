2bb2e44911a490b4d6768ae267a651b10c291522
Maxime Moreillon <m.moreillon@gmail.com>
2025-04-19 07:55:38 +0900

    filters fixed

--- components/garments/filters.tsx
+++ components/garments/filters.tsx
@@ -106,9 +106,9 @@ export default function GarmentsFilters(props: Props) {
         className="flex flex-col gap-2"
       >
         <div className="flex gap-4">
-          {Object.keys(filters).map((f, i) => (
+          {Object.keys(filters).map((f) => (
             <FormField
-              key={i}
+              key={f}
               control={form.control}
               name={f as keyof typeof filters}
               render={({ field }) => (
