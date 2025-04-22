6d025bc102ee187d5acbf1b6371fc3cef185d5f5
Maxime Moreillon <m.moreillon@gmail.com>
2025-04-22 12:58:48 +0900

    cicd

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   experimental: {
     serverActions: {
       bodySizeLimit: "10mb",
     },
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 };
 
