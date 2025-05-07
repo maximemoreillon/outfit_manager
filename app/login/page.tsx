6b3bb5dc272e59cb4c0fc1847d135adf25045236
Maxime Moreillon <m.moreillon@gmail.com>
2025-05-08 07:46:36 +0900

    login redirect

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,22 +1,16 @@
 import { signIn } from "@/auth";
-import { auth } from "@/auth";
 import { Button } from "@/components/ui/button";
-import { redirect } from "next/navigation";
 
 export default async function Login() {
-  const session = await auth();
-
-  if (session) redirect("/");
-  else
-    return (
-      <form
-        className="flex justify-center"
-        action={async () => {
-          "use server";
-          await signIn("auth0");
-        }}
-      >
-        <Button type="submit">Signin</Button>
-      </form>
-    );
+  return (
+    <form
+      className="flex justify-center"
+      action={async () => {
+        "use server";
+        await signIn("auth0", { redirectTo: "/" });
+      }}
+    >
+      <Button type="submit">Signin</Button>
+    </form>
+  );
 }
