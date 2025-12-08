838363d2679e35ff76080518c9ed5b9e9952a709
Maxime Moreillon <m.moreillon@gmail.com>
2025-12-09 08:13:38 +0900

    fixed api/images/fileKey

--- app/api/images/[...fileKey]/route.ts
+++ app/api/images/[...fileKey]/route.ts
@@ -1,13 +1,13 @@
 import { S3_BUCKET, s3Client } from "@/lib/s3";
 
-type Params = Promise<{ key: string }>;
+type Params = Promise<{ fileKey: string | string[] }>;
 export async function GET(_: Request, { params }: { params: Params }) {
-  let { key } = await params;
+  let { fileKey } = await params;
 
   // Not sure if this is needed
-  if (Array.isArray(key)) key = key.join("/");
+  if (Array.isArray(fileKey)) fileKey = fileKey.join("/");
 
-  const stream = await s3Client.getObject(S3_BUCKET, key);
+  const stream = await s3Client.getObject(S3_BUCKET, fileKey);
 
   if (!stream) throw new Error("No stream available");
 
